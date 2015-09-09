87202653dbabe21fdb15eb344d6c3724b1bc1c71
BraZee <oddoyedavid@gmail.com>
2015-09-09 23:10:00 +0000

    Test spreadsheet integration

--- contact_me.php
+++ contact_me.php
@@ -1,7 +1,7 @@
 <?php
 if($_POST) {
 
-    $to_Email = "info@heijulor.com"; //Replace with recipient email address
+    //$to_Email = "info@heijulor.com"; //Replace with recipient email address
 
     //check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
@@ -17,38 +17,53 @@ if($_POST) {
     }
 
     //check $_POST vars are set, exit if any missing
-    if(!isset($_POST["userName"]) || !isset($_POST["userEmail"]) || !isset($_POST["userSubject"]) || !isset($_POST["userMessage"])) {
+    if(!isset($_POST["Name"]) || !isset($_POST["Email"]) || !isset($_POST["RentBuy"]) || !isset($_POST["Area"])) {
         $output = json_encode(array('type'=>'error', 'text' => 'Input fields are empty!'));
         die($output);
     }
 
     //additional php validation
-    if(empty($_POST["userName"])) {
+    if(empty($_POST["Name"])) {
         $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
         die($output);
     }
-    if(!filter_var($_POST["userEmail"], FILTER_VALIDATE_EMAIL)) {
+    if(!filter_var($_POST["Email"], FILTER_VALIDATE_EMAIL)) {
         $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
         die($output);
     }
-    if(strlen($_POST["userMessage"])<5) {
-        $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
+    if(empty($_POST["Area"])) {
+        $output = json_encode(array('type'=>'error', 'text' => 'Area is empty!'));
         die($output);
     }
 
     //proceed with PHP email.
-    $headers = 'From: '.$_POST["userEmail"].'' . "\r\n" .
-    'Reply-To: '.$_POST["userEmail"].'' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+    // $headers = 'From: '.$_POST["Email"].'' . "\r\n" .
+    // 'Reply-To: '.$_POST["Email"].'' . "\r\n" .
+    // 'X-Mailer: PHP/' . phpversion();
 
         // send mail
-    $sentMail = @mail($to_Email, $_POST["userSubject"], $_POST["userMessage"] .'  -'.$_POST["userName"], $headers);
+    // $sentMail = @mail($to_Email, $_POST["RentBuy"], $_POST["Area"] .'  -'.$_POST["Name"], $headers);
+    //
+    // if(!$sentMail) {
+    //     $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+    //     die($output);
+    // } else {
+    //     $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$_POST["Name"] .' Thank you for your email'));
+    //     die($output);
+    // }
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'http://88.208.218.62:80/GeniusAPI/api/Song/AddEntry');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
 
     if(!$sentMail) {
         $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
         die($output);
     } else {
-        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$_POST["userName"] .' Thank you for your email'));
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$_POST["Name"] .' Thank you. We\'ll contact you shortly.'));
         die($output);
     }
 }
--- js/form.js
+++ js/form.js
@@ -5,38 +5,46 @@ $(document).ready(function() {
         //get input field values
         var user_name       = $('input[name=name]').val();
         var user_email      = $('input[name=email]').val();
-        var user_subject    = $('input[name=subject]').val();
-        var user_message    = $('textarea[name=message]').val();
-       
-        //data to be sent to server
-        post_data = {'userName':user_name, 'userEmail':user_email, 'userSubject':user_subject, 'userMessage':user_message};
-       
-        //Ajax post data to server
-        $.post('contact_me.php', post_data, function(response){  
-           
-            //load json data from server and output message    
-            if(response.type == 'error') {
+        var buy_rent    = $('input[name=rentbuy]').val();
+        var general_area    = $('input[name=area]').val();
+        //var user_message    = $('textarea[name=message]').val();
 
+        //data to be sent to server
+        post_data = post_data = {
+          'Name': user_name,
+          'Email': user_email,
+          'RentBuy': buy_rent,
+          'Area': general_area
+        };
+
+        console.log(post_data);
+
+        //Ajax post data to server
+        $.post('contact_me.php', post_data, function(response){
+
+            //load json data from server and output message
+            if(response.type == 'error') {
+                console.log('error');
                 output = '<div class="error-message"><p class="from">'+response.text+'</p></div>';
-                
+
             } else {
-           
+                console.log('no error');
                 output = '<div class="success-message"><p class="seuccses">'+response.text+'</p></div>';
-               
+
                 //reset values in all input fields
                 $('#email-form input').val('');
                 $('#email-form textarea').val('');
             }
-           
+
             $("#result").hide().html(output).slideDown();
 
         }, 'json');
 
     });
-   
+
     //reset previously set border colors and hide all message on .keyup()
     $("#email-form input, #email-form textarea").keyup(function() {
         $("#result").slideUp();
     });
-   
+
 });
