44713a7e525f138a9d307d287c65168d5cd46324
BraZee <oddoyedavid@gmail.com>
2015-09-10 00:07:20 +0000

    Spreadsheet integration working

--- js/form.js
+++ js/form.js
@@ -1,50 +1,78 @@
 $(document).ready(function() {
 
-    $("#email-form [type='submit']").click(function(event) {
-        event.preventDefault();
-        //get input field values
-        var user_name       = $('input[name=name]').val();
-        var user_email      = $('input[name=email]').val();
-        var buy_rent    = $('input[name=rentbuy]').val();
-        var general_area    = $('input[name=area]').val();
-        //var user_message    = $('textarea[name=message]').val();
+  function isValidEmailAddress(emailAddress) {
+      var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+      return pattern.test(emailAddress);
+  };
 
-        //data to be sent to server
-        post_data = post_data = {
-          'Name': user_name,
-          'Email': user_email,
-          'RentBuy': buy_rent,
-          'Area': general_area
-        };
+  $("#email-form [type='submit']").click(function(event) {
+    event.preventDefault();
+    //get input field values
+    var user_name       = $('input[name=name]').val();
+    var user_email      = $('input[name=email]').val();
+    var buy_rent    = $('input[name=rentbuy]').val();
+    var general_area    = $('input[name=area]').val();
+    //var user_message    = $('textarea[name=message]').val();
 
-        console.log(post_data);
+    if(user_name == ""){
+      output = '<div class="error-message"><p class="from">Name is empty!</p></div>';
+      $("#result").hide().html(output).slideDown();
+      return;
+    }
 
-        //Ajax post data to server
-        $.post('contact_me.php', post_data, function(response){
+    if(!isValidEmailAddress(user_email)){
+      output = '<div class="error-message"><p class="from">Please enter a valid email!</p></div>';
+      $("#result").hide().html(output).slideDown();
+      return;
+    }
+    if(buy_rent == ""){
+      output = '<div class="error-message"><p class="from">Please enter Buy or Rent!</p></div>';
+      $("#result").hide().html(output).slideDown();
+      return;
+    }
+    if(general_area == ""){
+      output = '<div class="error-message"><p class="from">Area is empty!</p></div>';
+      $("#result").hide().html(output).slideDown();
+      return;
+    }
 
-            //load json data from server and output message
-            if(response.type == 'error') {
-                console.log('error');
-                output = '<div class="error-message"><p class="from">'+response.text+'</p></div>';
+    //data to be sent to server
+    post_data = post_data = {
+      'Name': user_name,
+      'Email': user_email,
+      'RentBuy': buy_rent,
+      'Area': general_area
+    };
 
-            } else {
-                console.log('no error');
-                output = '<div class="success-message"><p class="seuccses">'+response.text+'</p></div>';
+    console.log(post_data);
 
-                //reset values in all input fields
-                $('#email-form input').val('');
-                $('#email-form textarea').val('');
-            }
 
-            $("#result").hide().html(output).slideDown();
+    //Ajax post data to server
+    $.post('http://88.208.218.62:80/GeniusAPI/api/Song/AddEntry', post_data, function(response){
 
-        }, 'json');
+      //load json data from server and output message
+      if(response != 200) {
+        console.log('error');
+        output = '<div class="error-message"><p class="from">'+response.text+'</p></div>';
 
-    });
+      } else {
+        console.log('no error');
+        output = '<div class="success-message"><p class="seuccses">'+response.text+'</p></div>';
 
-    //reset previously set border colors and hide all message on .keyup()
-    $("#email-form input, #email-form textarea").keyup(function() {
-        $("#result").slideUp();
-    });
+        //reset values in all input fields
+        $('#email-form input').val('');
+        $('#email-form textarea').val('');
+      }
+
+      $("#result").hide().html(output).slideDown();
+
+    }, 'json');
+
+  });
+
+  //reset previously set border colors and hide all message on .keyup()
+  $("#email-form input, #email-form textarea").keyup(function() {
+    $("#result").slideUp();
+  });
 
 });
